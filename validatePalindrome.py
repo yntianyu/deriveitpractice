f6a52de0836d63db2d808e86c981246b2cf96545
Elizabeth Yan <161661526+yntianyu@users.noreply.github.com>
2024-08-19 23:55:34 -0700

    validatePalindrome (8/18/2024)

---
+++ validatePalindrome.py
@@ -0,0 +1,25 @@
+#Given a string s, return whether the string is a palindrome.
+
+#A "palindrome" is a string that's the same forwards and backwards.
+
+def isValid(s):
+    l = 0
+    r = len(s) - 1
+
+    while l <= r:
+        if s[l] != s[r]:
+            return False
+        
+        l += 1
+        r -= 1
+        
+    return True
+
+
+
+isValid('racecar')
+isValid('a')
+isValid('ab')
+
+# Time complexity: O(n)
+# Space complexity: O(1)
