8fe69577d706e547767f1306651b6717b656895d
yntianyu <elizabeth.rose.yan@gmail.com>
2024-08-30 22:37:47 -0700

    cloneTree (??/??/2024)

--- cloneTree.py
+++ cloneTree.py
@@ -3,14 +3,12 @@
 # A "cloned tree" is defined as an exact copy. When you modify the cloned tree, it should have no effect on the original tree. This means that you can't simply return the original root node as the solution to the problem.
 
 
-"""
 # Here's the definition of a TreeNode:
 class TreeNode:
     def __init__(self, val=None, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-"""
 
 
 def cloneTree(root):
