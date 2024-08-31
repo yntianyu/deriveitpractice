0423c31a033c064f2d438a061b3b3b589909221b
yntianyu <elizabeth.rose.yan@gmail.com>
2024-08-30 22:41:58 -0700

    reverseLinkedList (8/21/2024)

--- reverseLinkedList.py
+++ reverseLinkedList.py
@@ -1,5 +1,13 @@
 # Given the head of a singly linked list head, reverse the list, and return the new head.
 
+
+# Here's the definition of a ListNode:
+class ListNode:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+
+
 def reverseList(head):
     prev = None
     current = head
@@ -11,6 +19,10 @@ def reverseList(head):
         current = next_node
 
     return prev
+
+reverseList(ListNode.of([1]))
+reverseList(ListNode.of([1, 2]))
+reverseList(ListNode.of([1, 2, 3]))
 
 # Time complexity: O(n)
 # Space complexity: O(1)
