060eb34551264865f48b0d911fa39fcb75fe54cf
yntianyu <elizabeth.rose.yan@gmail.com>
2024-08-30 22:44:11 -0700

    invertTree (8/30/2024)

--- invertTree.py
+++ invertTree.py
@@ -2,6 +2,15 @@
 
 # You should modify the tree in-place, and should not return anything.
 
+
+# Here's the definition of a TreeNode:
+class TreeNode:
+    def __init__(self, val=None, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+        
 def invertAll(root):
     if root is None:
         return
@@ -9,6 +18,10 @@ def invertAll(root):
     root.left, root.right = root.right, root.left
     invertAll(root.left)
     invertAll(root.right)
+
+invertAll(TreeNode.of([1, 2, 3, None, None, None, 4]))
+invertAll(TreeNode.of([1, 2]))
+invertAll(TreeNode.of([1, None, 2]))
 
 # Time complexity: O(n)
 # Space complexity: O(n) due to recursion stack
